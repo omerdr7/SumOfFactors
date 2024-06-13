85d565f786c8ddb6798d9e8b50a5780c10c57e31
omerdr7 <omerfarukdere32@gmail.com>
2024-06-13 20:02:27 +0300

    Sum of factors method created.

--- src/sumoffactors/SumOfFactors.java
+++ src/sumoffactors/SumOfFactors.java
@@ -1,12 +1,20 @@
 package sumoffactors;
 
-
 public class SumOfFactors {
 
-    
-    public static void main(String[] args) {
-        
-        
+    public static int sumFactors(int sayi) {
+        int sum = 0;
+
+        for (int i = 1; i <= sayi; i++) {
+            if (sayi % i == 0) {
+                sum += i;
+            }
+        }
+        return sum;
     }
-    
+
+    public static void main(String[] args) {
+
+    }
+
 }
